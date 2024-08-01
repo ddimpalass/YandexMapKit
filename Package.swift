0cf4bd19911dd13510f6970fd5fb9e62ae8d04e3
Dmitry Schelochkov <Schelochkov.Dmitriy@zolotoy.ru>
2024-08-01 22:39:08 +0300

    add binary package

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "YandexMapsMobileBinary",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        .library(
+            name: "YandexMapsMobileBinary",
+            targets: ["YandexMapsMobileBinary"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "YandexMapsMobileBinary",
+            url: "https://maps-ios-pods-public.s3.yandex.net/YandexMapsMobile-4.4.0-full.framework.zip",
+            checksum: "e5ccf772a1d196c5f1ac4bdb353a4aae4183933183dd7207ded4ba851c92dbb5"
+        )
+    ]
+)
