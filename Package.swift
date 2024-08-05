1d5e0b291936cec0f948ad6dc12c789ad6a7f830
Dmitry Schelochkov <Schelochkov.Dmitriy@zolotoy.ru>
2024-08-05 10:35:48 +0300

    rename

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "YandexMapsMobileBinary",
+    name: "YandexMapsMobile",
     platforms: [
         .iOS(.v15),
     ],
     products: [
         .library(
-            name: "YandexMapsMobileBinary",
-            targets: ["YandexMapsMobileBinary"]
+            name: "YandexMapsMobile",
+            targets: ["YandexMapsMobile"]
         ),
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "YandexMapsMobileBinary",
+            name: "YandexMapsMobile",
             url: "https://maps-ios-pods-public.s3.yandex.net/YandexMapsMobile-4.4.0-full.framework.zip",
             checksum: "e5ccf772a1d196c5f1ac4bdb353a4aae4183933183dd7207ded4ba851c92dbb5"
         )
